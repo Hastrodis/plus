6af21c606ef6863dffb5b8e1fa2d8d17c6d07c5e
Therion1 <i_babak@bk.ru>
2020-05-28 18:53:43 +0700

    Проверки функций
    несколько экспериментальных моментов

--- Convert/Convert.cpp
+++ Convert/Convert.cpp
@@ -64,8 +64,6 @@ char digit(int num)
     }
 }
 
-
-
 int main()
 {
 	//a - число, а1 - целая часть, а2 - дробная часть.
@@ -78,73 +76,43 @@ int main()
 		setlocale(LC_ALL, "Russian");
 		cout << "Введите число \n";
 		cin >> a;
-		if (a == 0) throw new NumberNotNull;
 		cout << "Введите систему счисления" << endl;
 		cin >> b;
+		cout << (int)a % b << endl;
+		/*
+		if (a == 0) throw new NumberNotNull;
+		//round(a, &a1, &a2);
+
+		cout << "Введите систему счисления" << endl;
+		cin >> b;*/
+
 		if (b < 2) {
 			throw new NumberSystemBase();
 		}
+		/*
 		try {
 		
 		}
 		catch (NumberSystemBase* ex) {
 			cout << ex->getMessage() << " Code: " << ex->getCode();
-		}
+		}*/
 	}
 	catch (NumberNotNull* ex) {
 		cout << ex->getMessage() << " Code: " << ex->getCode();
 	}
     
 	
-	
-
-	//cout << "Дробная часть от a = " << drob(a, b) << endl;
-	//-------------------------------------дробная часть-------------------------------------
-	//drob(a2, b, string);
-	//cout << string << endl;
 }
 
 //делит на целую и дробную
-void round(double a, double* a1, double* a2) {
+void round(double a, double* a1, double* a2) 
+{
 	*a2 = modf(a, a1);
 }
 
-//<<<<<<< HEAD
-//замена числа на цифру, к примеру switch(10) = A
-char digit(int num)
-{
-    switch (num) {
-    case 0: return '0';
-    case 1: return '1';
-    case 2: return '2';
-    case 3: return '3';
-    case 4: return '4';
-    case 5: return '5';
-    case 6: return '6';
-    case 7: return '7';
-    case 8: return '8';
-    case 9: return '9';
-    case 10: return 'A';
-    case 11: return 'B';
-    case 12: return 'C';
-    case 13: return 'D';
-    case 14: return 'E';
-    case 15: return 'F';
-    }
-}
 //=======
 //это вроде как классы исключений для вывода ошибок (сверху прописаны)
 
-class NumberError
-    {
-
-    };
-class NumberSystemBase :NumberError {
-public:
-	virtual const char* getMessage() { return NUMBER_SYSTEM_BASE_TEXT; };
-	virtual int getCode() { return NUMBER_SYSTEM_BASE_CODE; };
-};
-//>>>>>>> bc4600ce1d8fe6d74e4cf812ae6a757a6ea702dc
 
 
 //другая вариация для целочисленной части
@@ -161,8 +129,6 @@ char drob(double a, int p) {
 		}
 		//s = '1';
 		//s[k] = digit(1);
-	
-	
 }
 
 //целочисленная часть, суть в том, что полученное число отправляется в s по указателю
