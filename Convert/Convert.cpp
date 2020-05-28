70c6562cfaf72e08d29c88c40c32febf2cda6ed2
Therion1 <i_babak@bk.ru>
2020-05-29 00:48:01 +0700

    Конвертация (последние изменения)
    Доделана основная процедура main()
    составлены функции разделения и перевода

--- Convert/Convert.cpp
+++ Convert/Convert.cpp
@@ -4,12 +4,10 @@
 
 
 #define NUMBER_SYSTEM_BASE_CODE -1;
-#define NUMBER_SYSTEM_BASE_TEXT "Система счислений меньше 2!"; 
-//#define NUMBER_SYSTEM_BASE_TEXT "The base of the number system is less than 2!";
+#define NUMBER_SYSTEM_BASE_TEXT "Система счисления меньше 2!"; 
 
 #define NUMBER_NOT_NULL_CODE 101;
 #define NUMBER_NOT_NULL_TEXT "Число не может быть 0!";  
-//#define NUMBER_NOT_NULL_TEXT "The number must not be zero!";
 //это вроде как классы исключений для вывода ошибок (сверху прописаны)
 
 //класс ошибки 
@@ -37,11 +35,12 @@ using namespace std;
 //char digit(int num){}
 //делит на целую и дробную
 void round(double a, double* a1, double* a2);
-char drob(double a, int p);
+void drob(double a, int p, char* s);
+void drob(double a, int p, char* s, int it1);
 int integerPart(int a, int p, char* s);
 
 
-//замена числа на цифру, к примеру switch(10) = A
+//замена чисел типа int на цифры/буквы типа char
 char digit(int num)
 {
     switch (num) {
@@ -74,28 +73,42 @@ int main()
 	char s[100] = { 0 };
 	try {
 		setlocale(LC_ALL, "Russian");
-		cout << "Введите число \n";
+		cout << "Введите число: \n";
 		cin >> a;
-		cout << "Введите систему счисления" << endl;
-		cin >> b;
-		cout << (int)a % b << endl;
-		/*
 		if (a == 0) throw new NumberNotNull;
-		//round(a, &a1, &a2);
-
-		cout << "Введите систему счисления" << endl;
-		cin >> b;*/
-
+		//разделение на двое
+		round(a, &a1, &a2);
+		cout << "Введите систему счисления:" << endl;
+		cin >> b;
+		cout << "" << endl;
 		if (b < 2) {
 			throw new NumberSystemBase();
 		}
-		/*
-		try {
 		
+		try {
+			//преобразование целой части
+			int k = integerPart(a1, b, s); //k счетчик
+			//it1 используется как двойное условие - для проверки и для округления
+			int it1 = a - (int)a;
+			if (it1 != 0) {
+				//преобразование вещественной части
+				cout << "Введите точность округления \n (в случае если точность будет равна 0 или меньше - у переведенного числа вещественнаяая часть будет полной): \n";
+				cin >> it1;
+				if (it1 > 0) {
+					s[k++] = ',';
+					drob(a2, b, &s[k], it1);
+
+				}
+				else {
+					drob(a2, b, &s[k]);
+				}
+			}
+			else s[k++] = '\0'; //в случае если число 'a' целое - добавляем детерминирующий ноль (вроде так называется)
+			cout << "Ответ: \n		Число " << a << " в десятичной системе счисления = " << s << " в двоичной системе счисления" << endl;
 		}
 		catch (NumberSystemBase* ex) {
 			cout << ex->getMessage() << " Code: " << ex->getCode();
-		}*/
+		}
 	}
 	catch (NumberNotNull* ex) {
 		cout << ex->getMessage() << " Code: " << ex->getCode();
@@ -114,45 +127,55 @@ void round(double a, double* a1, double* a2)
 //это вроде как классы исключений для вывода ошибок (сверху прописаны)
 
 
-
-//другая вариация для целочисленной части
-
-
 //Дробная часть - преобразование
-char drob(double a, int p) {
-	int k = 0;
-	double g = 0;
-		a *= p;
-		if (a > (p-1)) {
-			return digit((int)a);
-			round(a, &g, &a);
-		}
-		//s = '1';
-		//s[k] = digit(1);
+//условное ограничение
+void drob(double a, int b, char* s) {
+	int it = 0, k = 0;
+	double a1 = a;
+	do {
+		a1 *= b;
+		s[k++] = digit((int)a1);
+		a1 -= (int)a1;
+		it++;
+	} while (a1 > 0.00000001);
+	s[k] = '\0'; //конец строки
+}
+//с ограничением
+void drob(double a, int b, char* s, int it1) {
+	int it = 0, k = 0;
+	double a1 = a;
+	do {
+		a1 *= b;
+		s[k++] = digit((int)a1);
+		a1 -= (int)a1;
+		it++;
+	} while (it < it1); //ограничение через задание предела (кол-во знаков после запятой)
+	s[k] = '\0'; //конец строки
 }
 
 //целочисленная часть, суть в том, что полученное число отправляется в s по указателю
-int integerPart(int a, int p, char* s) {
+int integerPart(int a, int b, char* s) {
 
-    if (p < 2) {
+    if (b < 2) {
         //throw new NumberSystemBase();
 		cout << "Ошибка, система счисления меньше 2" << endl;
     }
 	//берется целочисленная часть
     int num = (int)a;
 	//остаток от деления на p (систему счисления)
-    int rest = num % p;
+    int rest = num % b;
 	//num делится на p и сохраняется (типо num = num / p)
-    num /= p;
+    num /= b;
     if (num == 0)
     {
+		//когда рекурсия дойдет до конца, начнет возвращать значения
         s[0] = digit(rest); return 1;
     }
-    int k = integerPart(num, p, s);
+    int k = integerPart(num, b, s);
     s[k++] = digit(rest);
     return k;
 }
 
 
-// https://prog-cpp.ru/notation/ я тут нашел как раз то, что у макса
+
 
