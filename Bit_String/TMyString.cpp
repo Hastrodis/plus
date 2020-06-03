2a81825c6759ffe1124ecea0ee9bd242449da3e6
Therion1 <i_babak@bk.ru>
2020-06-03 12:11:48 +0700

    доработки

--- Bit_String/TMyString.cpp
+++ Bit_String/TMyString.cpp
@@ -1,1 +1,19 @@
 #include "TMyString.h"
+
+TMyString::TMyString() {
+
+}
+
+TMyString::~TMyString() {
+
+}
+
+/*TMyString::int getSize() {
+	return 0;
+}
+
+TMyString::void setSize(int newSize) {}
+
+TMyString::void setChar(int pos, char ch) {}
+
+TMyString::char operator[](int i) {}*/
